7d005efbbd12c35ddba3471a5f943879cb110d6d
Jorge338991 <jorge338991@gmail.com>
2024-11-14 13:23:12 +0100

    Se añaden mas invocaciones desde el main ajustandose al ejercicio propuesto.

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -1,11 +1,34 @@
 package com.campusdual.classroom;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Exercise11 {
     public static void main(String[] args) {
         TVRemote redRemote = new TVRemote("Rojo");
         TVRemote blackRemote = new TVRemote("Negro");
+
+
+        System.out.println(redRemote.turnOn());
+        System.out.println(redRemote.channel);
+        redRemote.channelUp();
+        System.out.println(redRemote.channel);
+        redRemote.channelDown();
+        System.out.println(redRemote.channel);
+        redRemote.channelDown();
+        System.out.println(redRemote.channel);
+        redRemote.volumeUp();
+        redRemote.volumeDown();
+        System.out.println(redRemote.volume);
+        redRemote.volume=0;
+        System.out.println(redRemote.volume);
+        redRemote.volumeDown();
+        System.out.println(redRemote.volume);
         System.out.println(redRemote.getColor());
-        System.out.println(blackRemote.getColor());
+        System.out.println(redRemote.turnOff());
+
+
+
+
     }
 
 }
